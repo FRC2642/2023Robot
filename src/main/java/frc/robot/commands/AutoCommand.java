8a41c90d69c6d8ecf3e3665a7cb60a8fe22fa839
Tricosmos <karnekaa@students.pittschools.org>
2023-02-05 14:54:14 -0500

    Code

--- src/main/java/frc/robot/commands/AutoCommand.java
+++ src/main/java/frc/robot/commands/AutoCommand.java
@@ -4,7 +4,11 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.Robot;
 import frc.robot.RobotContainer;
 
 public class AutoCommand extends CommandBase {
@@ -13,28 +17,39 @@ public class AutoCommand extends CommandBase {
     // Use addRequirements() here to declare subsystem dependencies.
 
     addRequirements(RobotContainer.DriveSub);
-
+  
   }
-
+  final PIDController PID = new PIDController(.02, 0, 0);
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+
+    RobotContainer.DriveSub.GyroYawReset();
+    RobotContainer.DriveSub.EncoderReset();
+    
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
 
-    RobotContainer.DriveSub.arcadeDrive(0.25, 0.25);
+    
+    RobotContainer.DriveSub.arcadeDrive(-0.25, RobotContainer.DriveSub.GetYaw());
+    double GetYaw = RobotContainer.DriveSub.GetYaw();
+    double GetTicks = RobotContainer.DriveSub.GetLeftTicks();
 
+    double turnspeed = MathUtil.clamp(PID.calculate(GetYaw,0), -1, 1);
+
+    
+    RobotContainer.DriveSub.arcadeDrive(-.25, turnspeed);
   }
-
+  
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
+  public boolean IsLeftFinished() {
+    return RobotContainer.DriveSub.GetLeftTicks() <= -15;
   }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -8,21 +8,31 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+
+import com.ctre.phoenix.sensors.PigeonIMU;
+import com.ctre.phoenix.sensors.WPI_Pigeon2;
+import com.ctre.phoenixpro.hardware.Pigeon2;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 
 public class DriveSubsystem extends SubsystemBase {
   /** Creates a new DriveSubsystem. */
   
   
-
+ 
     CANSparkMax FLMotor = new CANSparkMax(Constants.FL, MotorType.kBrushless);
     CANSparkMax BLMotor = new CANSparkMax(Constants.BL, MotorType.kBrushless);
     CANSparkMax FRMotor = new CANSparkMax(Constants.FR, MotorType.kBrushless);
     CANSparkMax BRMotor = new CANSparkMax(Constants.BR, MotorType.kBrushless);
 
+    
     RelativeEncoder FRencoder = FRMotor.getEncoder();
+    RelativeEncoder FLencoder = FLMotor.getEncoder();
+    WPI_Pigeon2 Gyro = new WPI_Pigeon2(18);
+   
+    
 
     MotorControllerGroup RightMotors= new MotorControllerGroup(FRMotor, BRMotor);
     MotorControllerGroup LeftMotors= new MotorControllerGroup(FLMotor, BLMotor);
@@ -37,15 +47,33 @@ public class DriveSubsystem extends SubsystemBase {
     DDrive.arcadeDrive(movespeed, rotatespeed);
   }
 
-  public double getEncoderTicks(){
+  public double GetRightTicks(){
 
     return FRencoder.getPosition();
   }
 
+  public double GetLeftTicks(){
 
+    return FLencoder.getPosition();
+  }
+
+  public double GetYaw(){
+
+    return Gyro.getYaw();
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+  public void GyroYawReset() {
+
+    Gyro.setYaw(0);
+  }
+
+  public void EncoderReset() {
+
+    FRencoder.setPosition(0);
+    FLencoder.setPosition(0);
   }
 }
