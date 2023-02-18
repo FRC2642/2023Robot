2a29bdf4a05071ca8a63f14abab36204fbbe0bec
draketh818199 <thomas.a.drake2005@gmail.com>
2023-02-17 19:06:13 -0500

    fix

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -80,16 +80,5 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     return testPathFollowCommand;// FollowPathVisionRecenterCommand(new RecenterDisplacementCommand(limelight), testPathFollowCommand);
 
-import edu.wpi.first.wpilibj2.command.Command;
-
-public class RobotContainer {
-      
-  public RobotContainer() {
-    
-  }
-
-  public Command getAutonomousCommand() {
-    return null;
-
   }
 }
