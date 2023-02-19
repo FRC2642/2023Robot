a95c7f436dca3b297bed637e6085d607c13f5a99
draketh818199 <thomas.a.drake2005@gmail.com>
2023-02-19 13:54:47 -0500

    carriage is now controled by right aux joystick

--- src/main/java/frc/robot/commands/teleop/MastCommands/CarriageMoveCommand.java
+++ src/main/java/frc/robot/commands/teleop/MastCommands/CarriageMoveCommand.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.teleop.ClawCommands;
+package frc.robot.commands.teleop.MastCommands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj.XboxController;
@@ -29,18 +29,12 @@ public class CarriageMoveCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
-    //left bumper moves down right moves up
-    if (aux.getLeftBumper()){
-      carriage.moveCarriage(.1);
-    }
-    else if (aux.getRightBumper()) {
-      carriage.moveCarriage(-.1);
+    if (aux.getLeftTriggerAxis() != 0){
+        carriage.moveCarriage(aux.getLeftTriggerAxis());
     }
     else {
       carriage.moveCarriage(0);
     }
-    
   }
 
   // Called once the command ends or is interrupted.
